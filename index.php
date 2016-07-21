aab67e7ca5f2ab4d8971876b199ea4de6ded813f
Devansh Goenka <devanshgoenka97@gmail.com>
2016-07-21 16:30:55 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!Doctype HTML>
+<html>
+<head>
+	<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<div class='wrapper'>
+		<div class='header-container'>
+			<div class='logo-container'>
+				<div class='logo'>
+				</div>
+			</div>
+			<div class='top-strip'>
+				<i class='fa fa-facebook'></i>
+				<i class='fa fa-twitter'></i>
+				<i class='fa fa-google-plus'></i>
+				<i class='fa fa-linkedin'></i>
+			</div>
+			<div class='mid-strip'>
+				National Institute of Technology, Durgapur
+			</div>
+			<div class='bottom-strip'>
+
+			</div>
+		</div>
+        <div class='content-container'>
+        	<div class='student-notice-board-container'>
+        		<div class='student-notice-board-header'>
+        		Student Notice Board
+        			<hr>
+        		</div>
+        		<ul>
+        			<li>
+        		    <div class='student-notice-board-content'>
+        			Notice 1
+        		    </div>
+        		    </li>
+        		    <li>
+        		    <div class='student-notice-board-content'>
+        			Notice 2
+        		    </div>
+        		    </li>
+        	    </ul>
+        	</div>
+        	<div class='result-container'>
+        		<div class='result-content'>
+        		</div>
+        	</div>
+        </div>
+	</div>	
+</body>
+</html>
